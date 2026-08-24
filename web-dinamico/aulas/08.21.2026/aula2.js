b85ee5b2439014e47e3a8e62dc91f9b88af62033
Kaimy Azambuja <138644466+FlyVz@users.noreply.github.com>
2026-08-23 23:02:03 -0400

    .

---
+++ web-dinamico/aulas/08.21.2026/aula2.js
@@ -0,0 +1,41 @@
+// const nota = 7.5; 
+
+// if (nota >=6) {
+//     console.log("Aprovado");
+// } else if (nota >= 4) {
+//     console.log("Recuperação");
+// } else {
+//     console.log("Reprovado");
+// }
+
+//-----------------------------------------------------------
+
+// for ( variavel inicial; condição; incremento) {
+//     // bloco de código
+// }
+
+// for (let i= 1; i <= 5; i++) {
+//     console.log("volta" + i);
+// }
+
+//-----------------------------------------------------------
+
+// const num = 10;
+
+// for (i = 0; i <= 10; i ++) {
+//     console.log(num + " * " + i + " = " + (num * i))
+// }
+
+// for (i = num; i > 0; i --) {
+//     console.log(i)
+// }
+
+function ehPar(a) {
+    if (a % 2 == 0) {
+        console.log("'E par")
+    } else {
+        console.log("Impar")
+    }
+}
+
+ehPar(3)
